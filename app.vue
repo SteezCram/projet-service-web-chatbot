68d149139c6a34495fd48beee50f3b136f64814a
Thomas Croizet <46353105+SteezCram@users.noreply.github.com>
2023-04-04 09:29:35 +0200

    Add basic shell

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+    <NuxtLayout name="shell">
+        <NuxtPage/>
+    </NuxtLayout>
+</template>
---
+++ layouts/shell.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="w-screen h-screen bg-white dark:bg-black">
+        <header class="w-screen h-16 p-4 bg-teal-500 flex flex-row items-center">
+            <img class="w-8 h-8" src="/favicon.ico">
+
+            <nav>
+                <ul class="flex flex-row">
+                    <li>
+                        <nuxt-link to="/">Home</nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link to="/about">About</nuxt-link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <slot></slot>
+    </main>
+</template>
