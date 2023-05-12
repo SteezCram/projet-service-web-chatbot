0ebb9a5bea2abbed574f1bfc3cf84a11a57a8d44
vctr-dng <94930828+vctr-dng@users.noreply.github.com>
2023-05-12 16:59:19 +0200

    untested Rive Script manager

---
+++ api/models/riveScriptManager.js
@@ -0,0 +1,74 @@
+const databaseManager = require('./databaseManager')
+// chatbot_rivescript
+// ID, NAME, CONTENT
+
+module.exports.createRiveScript = async function (riveScriptData) {
+  let newRiveScriptID = -1
+  try {
+    let riveScriptName = "Unnamed riveScript"
+    if (riveScriptData.name){
+      riveScriptName = riveScriptData.name
+    }
+    const riveScriptDescription = riveScriptData.description
+    const riveScriptScript = riveScriptData.script
+
+    newRiveScriptID = await databaseManager.createRiveScript(riveScriptName, riveScriptContent)
+  } catch (err) {
+    console.error(err)
+  }
+  return newRiveScriptID
+}
+
+module.exports.updateRiveScript = async function (id, riveScriptData) {
+  try {
+    for (const key in riveScriptData) {
+      const res = await databaseManager.updateRiveScript(id, key, riveScriptData[key])
+      if (!res) {
+        return false
+      }
+    }
+    return true
+  } catch (err) {
+    console.error(err)
+    return false
+  }
+}
+
+module.exports.getAll = async function () {
+  let dbRequest
+  try {
+    dbRequest = await databaseManager.getRiveScripts()
+    if (!dbRequest) {
+        dbRequest = {}
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  return dbRequest
+}
+
+module.exports.getRiveScript = async function (id) {
+  let dbRequest
+  try {
+    dbRequest = await databaseManager.getRiveScript(id)
+    if (!dbRequest) {
+        dbRequest = {}
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  return dbRequest
+}
+
+module.exports.deleteRiveScript = async function (id) {
+  let dbRequest
+  try {
+    dbRequest = await databaseManager.deleteRiveScript(id)
+    if (!dbRequest) {
+        dbRequest = {}
+    }
+  } catch (err) {
+    console.error(err)
+  }
+  return dbRequest
+}
