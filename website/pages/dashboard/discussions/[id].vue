f68ec044481bc6a8817c5e00971acafcca70e00f
Thomas Croizet <46353105+SteezCram@users.noreply.github.com>
2023-05-26 15:09:43 +0200

    Add home page and fix UI

--- website/pages/dashboard/discussions/[id].vue
+++ website/pages/dashboard/discussions/[id].vue
@@ -63,14 +63,14 @@
                 <label for="chat" class="sr-only">Votre message</label>
 
                 <div class="flex items-center px-3 py-2 rounded-lg bg-gray-100 dark:bg-gray-900 shadow-md">
-                    <button @click="addEmoji" ref="emojiButton" type="button" class="p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
+                    <button @click="addEmoji" ref="emojiButton" type="button" class="sm:p-2 text-gray-500 rounded-lg cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
                         <svg aria-hidden="true" class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zm-.464 5.535a1 1 0 10-1.415-1.414 3 3 0 01-4.242 0 1 1 0 00-1.415 1.414 5 5 0 007.072 0z" clip-rule="evenodd"></path></svg>
                         <span class="sr-only">Ajouter un emoji</span>
                     </button>
 
-                    <textarea @keypress="enterKeyHandler($event)" v-model.trim="message" rows="1" class="block mx-4 p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-green-500 focus:border-primary-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" :placeholder="`Envoyer un message à ${bot_name}`"></textarea>
+                    <textarea @keypress="enterKeyHandler($event)" v-model.trim="message" rows="1" class="block mx-2 sm:mx-4 sm:p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-green-500 focus:border-primary-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" :placeholder="`Envoyer un message à ${bot_name}`"></textarea>
                     
-                    <button ref="submitButton" type="submit" class="inline-flex justify-center p-2 text-primary-600 rounded-full cursor-pointer hover:bg-primary-100 dark:text-primary-100 dark:hover:bg-primary-600">
+                    <button ref="submitButton" type="submit" class="inline-flex justify-center sm:p-2 text-primary-600 rounded-full cursor-pointer hover:bg-primary-100 dark:text-primary-100 dark:hover:bg-primary-600">
                         <svg aria-hidden="true" class="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
                         <span class="sr-only">Envoyer le message</span>
                     </button>
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-    
+    <contaier-full class="!h-full flex">
+        <container class="flex flex-col m-auto">
+            <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                Un nouveau <span class="text-primary-600 dark:text-primary-500">monde de conversation intelligente</span> avec nos chatbots.
+            </h1>
+
+            <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Découvrez un monde de conversation intelligente avec notre site de discussion de chatbot, où l'intelligence artificielle rencontre l'interaction humaine pour des expériences uniques et engageantes !</p>
+        
+            <div class="flex flex-row justify-center">
+                <btn-primary class="!w-auto mt-10 flex flex-row justify-center items-baseline">
+                    Démarrer maintenant
+                    <i class="ml-1 icon icon-arrow-right"></i>
+                </btn-primary>
+            </div>
+        </container>
+    </contaier-full>
 </template>
